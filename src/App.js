36dde058117e860b0c224a4a6acc1a8892899c55
Th3At0nic <islammdrahatul@gmail.com>
2021-05-16 19:49:55 +0600

    random commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
-    <div className="">
+    <>
       <Navbar />
-      <h1>Raht</h1>
-    </div>
+      <Header />
+    </>
   );
 }
 
