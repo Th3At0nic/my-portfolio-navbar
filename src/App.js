c50b79892572b6760fe97cd615f9ea6a3bf1f2fd
Th3At0nic <islammdrahatul@gmail.com>
2021-05-16 17:08:24 +0600

    implemented navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="">
+      <Navbar />
       <h1>Raht</h1>
     </div>
   );
